35ae04d9a1c69ae2ad16be35b24551e0cbf25f61
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-04-22 15:32:51 +0300

    add dotnet 4.6.2 and powershell 3

---
+++ scripts/install_dotnet4.6.2.ps1
@@ -0,0 +1,20 @@
+Write-Host 'Downloading DotNet 4.6.2...'
+
+$url = 'https://download.microsoft.com/download/F/9/4/F942F07D-F26F-4F30-B4E3-EBD54FABA377/NDP462-KB3151800-x86-x64-AllOS-ENU.exe'
+$filename = 'dotnet4.6.2.exe'
+$output = 'C:\temp'
+
+# create $output
+if (!(test-path $output))
+{
+    New-Item -ItemType directory -Path $output
+}
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, "$output\$filename")
+
+Write-Host 'Installing DotNet ...'
+
+Start-Process -FilePath "$output\$filename" -ArgumentList "/norestart /passive /log c:\temp\dotnet_install.log" -Wait
+
+Write-Host 'DotNet installed.'
